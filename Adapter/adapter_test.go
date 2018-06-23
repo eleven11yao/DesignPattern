3032ed45f7d1132404a44dbf3366f93b6bc2c19e
m11 <eleven11yao@qq.com>
2018-06-24 03:30:41 +0800

    update adapter

--- Adapter/adapter_test.go
+++ Adapter/adapter_test.go
@@ -6,8 +6,11 @@ import (
 )
 
 func TestAdapter(t *testing.T) {
+	var ada Adapter
 	fmt.Println("-----Start------")
-	fmt.Println(NewPigAdapter().Start())
-	fmt.Println(NewEagleAdapter().Start())
+	ada = NewPigAdapter()
+	fmt.Println(ada.Start())
+	ada = NewEagleAdapter()
+	fmt.Println(ada.Start())
 	fmt.Println("-----End------")
 }
